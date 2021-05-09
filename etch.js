f04c47e8295c851892ea08a322db081bbd1fe188
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-05-09 17:05:57 -0400

    deferred the javascript so that it would load properly

--- etch.js
+++ etch.js
@@ -1,1 +1,28 @@
-const containter = document.createElement('div')
+const container = document.getElementById('container');
+container.textContent = "hello_World!";
+container.style.display = "flex";
+
+
+function createBox(){
+    let box = document.createElement('div')
+    box.className = 'littleBox';
+    box.textContent = "Hello!"
+    container.appendChild(box);
+}
+
+let i = 0;
+while(i < 16){
+    createBox();
+    i++;
+}
+const littleBox = document.getElementsByClassName('littleBox');
+container.style.border = "thin solid #000";
+littleBox.textContent = "hello";
+
+
+
+
+
+
+
+
