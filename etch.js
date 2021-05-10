3f082ce376cb114bfeda626b1fd6ec6aaa23f7d9
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-05-10 14:19:24 -0400

    Update etch.js
    made the divs into a grid and added a hover function that changes the background color of the divs to light blue when mouse goes over them.

--- etch.js
+++ etch.js
@@ -1,23 +1,57 @@
-const container = document.getElementById('container');
-container.textContent = "hello_World!";
-container.style.display = "flex";
 
 
+
+container.style.width = "1000px";
+container.style.display = "grid";
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap';
+
+// create button element
+let button = document.createElement('button');
+container.appendChild(button);
+button.style.position ='float';
+button.style.float = 'right';
+button.textContent = 'Click me to clear everything';
+button.style.width = container.style.width;
+button.style.height = '90px';
+
+
+
+// function to create a box in the container
 function createBox(){
-    let box = document.createElement('div')
-    box.className = 'littleBox';
-    box.textContent = "Hello!"
+    let box = document.createElement('div');
+    box.className = "Box";
+    // box.textContent = 'Im a little Box';
+    box.style.width = '240px';
+    box.style.height = "240px";
+    box.style.border = '1px solid black';
+    box.style.display = 'flex';
+    box.addEventListener("mouseover", mouseOver);
+    function mouseOver(){
+        box.style.backgroundColor = 'lightblue';
+    }
     container.appendChild(box);
+
 }
 
-let i = 0;
-while(i < 16){
+// loop to create a number of boxes
+i = 0
+while(i < 16) {
     createBox();
     i++;
 }
-const littleBox = document.getElementsByClassName('littleBox');
-container.style.border = "thin solid #000";
-littleBox.textContent = "hello";
+
+button.style.backgroundColor = 'red';
+
+
+
+
+
+
+
+
+
+
 
 
 
