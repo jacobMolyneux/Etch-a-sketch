6c0ef5b38303c2a8c9b531b43ad29586b04c2f90
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-05-18 07:46:57 -0400

    mouse over now chooses a color at Random

--- etch.js
+++ etch.js
@@ -1,46 +1,25 @@
-
-
 const container = document.getElementById('container');
-container.style.width = "960px";
-container.style.display = "grid";
-container.style.display = 'flex';
-container.style.flexWrap = 'wrap';
-
-// create button element
-
-
-
-
-// function to create a box in the container
-function createBox(){
-    const box = document.createElement('div');
-    box.className = 'box';
-    container.appendChild(box);
-
+for(let i = 0; i < 16; i++){
     
+    const box = document.createElement('div');
+    box.classList = 'box';
+    box.addEventListener("mouseover", function(){
+        box.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);;
+    })
+    
+    box.addEventListener("mouseout", function(){
+        box.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);;
+    })
+    
+    container.appendChild(box);
 }
 
-// loop to create a number of boxes
-i = 0
-while(i < 16) {
-    createBox();
-    i++;
-}
-let box = document.getElementsByClassName('box');
-box.style.width = '50px';
-box.style.height = '50px';
-box.backgroundColor = 'Black'
+let restartButton = document.getElementById('restartButton');
 
-
-
-// button.style.backgroundColor = 'red';
-
-
-// box.addEventListener("mouseover", mouseOver);
-//     function mouseOver(){
-//         box.style.backgroundColor = 'lightblue';
-//     }
-//     container.appendChild(box);
+restartButton.addEventListener('click',function(){
+    const box = document.getElementsByClassName('box');
+    box.style.background = 'white';
+})
 
 
 
