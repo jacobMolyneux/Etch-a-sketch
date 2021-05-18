ff2600c2452b5d440377ec0abc8bab4dfbda6774
Jacob Molyneux <79246480+jacobMolyneux@users.noreply.github.com>
2021-05-18 05:42:40 -0400

    Update etch.js

--- etch.js
+++ etch.js
@@ -1,37 +1,23 @@
 
 
-
-container.style.width = "1000px";
+const container = document.getElementById('container');
+container.style.width = "960px";
 container.style.display = "grid";
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 
 // create button element
-let button = document.createElement('button');
-container.appendChild(button);
-button.style.position ='float';
-button.style.float = 'right';
-button.textContent = 'Click me to clear everything';
-button.style.width = container.style.width;
-button.style.height = '90px';
+
 
 
 
 // function to create a box in the container
 function createBox(){
-    let box = document.createElement('div');
-    box.className = "Box";
-    // box.textContent = 'Im a little Box';
-    box.style.width = '240px';
-    box.style.height = "240px";
-    box.style.border = '1px solid black';
-    box.style.display = 'flex';
-    box.addEventListener("mouseover", mouseOver);
-    function mouseOver(){
-        box.style.backgroundColor = 'lightblue';
-    }
+    const box = document.createElement('div');
+    box.className = 'box';
     container.appendChild(box);
 
+    
 }
 
 // loop to create a number of boxes
@@ -40,10 +26,21 @@ while(i < 16) {
     createBox();
     i++;
 }
+let box = document.getElementsByClassName('box');
+box.style.width = '50px';
+box.style.height = '50px';
+box.backgroundColor = 'Black'
 
-button.style.backgroundColor = 'red';
 
 
+// button.style.backgroundColor = 'red';
+
+
+// box.addEventListener("mouseover", mouseOver);
+//     function mouseOver(){
+//         box.style.backgroundColor = 'lightblue';
+//     }
+//     container.appendChild(box);
 
 
 
